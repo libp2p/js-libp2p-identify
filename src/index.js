25ce40f61186d0e3c785da95f78c99f7a71a9a26
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-12-08 12:00:53 +0100

    docs(api): first pass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+/**
+ * Identify is a STUN protocol, used by libp2p-swarmin order to
+ * broadcast and learn about the `ip:port` pairs a specific
+ * peer is available through and to know when a new stream
+ * muxer is established, so a conn can be reused.
+ *
+ * @module libp2p-identify
+ */
 'use strict'
 
 exports = module.exports
+
+/**
+ * @type {string}
+ */
 exports.multicodec = '/ipfs/id/1.0.0'
+
 exports.listener = require('./listener')
 exports.dialer = require('./dialer')
